f5aa13b9008dc9da6411f708616b2187555bcaca
oTrinhHoangHai <trinh.hoang.hai@framgia.com>
2019-06-04 09:23:05 +0700

    trash

--- customAVPlayer/ViewController.swift
+++ customAVPlayer/ViewController.swift
@@ -7,8 +7,25 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+//    let videoPath = Bundle.main.path(forResource: "bulletTrain", ofType: "mp4")!
+//    let videoPath = "https://wolverine.raywenderlich.com/content/ios/tutorials/video_streaming/foxVillage.mp4"
+
+    @IBAction func playButtonTap(_ sender: Any) {
+        let videoPath = Bundle.main.path(forResource: "bulletTrain", ofType: "mp4")!
+        let videoURL = URL(fileURLWithPath: videoPath)
+        let player = AVPlayer(url: videoURL)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+
+        present(playerViewController, animated: true) {
+            player.play()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
